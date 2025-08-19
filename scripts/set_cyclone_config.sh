0a77c4f13c317ebf6fee71fdc8ec59e20f121375
Daniel San José Pro <42489409+danielsanjosepro@users.noreply.github.com>
2025-08-19 13:22:55 +0200

    fixing camera / cyclone issues

--- scripts/set_cyclone_config.sh
+++ scripts/set_cyclone_config.sh
@@ -3,6 +3,7 @@ if [ -z "$ROS_NETWORK_INTERFACE" ]; then
     echo "ROS_NETWORK_INTERFACE is not set. Defaulting to 'lo' (loopback interface)."
     export ROS_NETWORK_INTERFACE=lo
 fi
-export CYCLONEDDS_URI=$HOME/ros2_ws/src/crisp_controllers_demos/config/cyclone_config.xml
+
+export CYCLONEDDS_URI=file:///home/ros/ros2_ws/src/crisp_controllers_demos/config/cyclone_config.xml
 
 ros2 daemon stop && ros2 daemon start
